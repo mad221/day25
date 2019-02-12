c481be44fbb2b47f774c90cf96fc9b580d53731c
MarionA29 <marionaudren@live.fr>
2019-02-12 19:13:04 +0100

    little changes

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,11 +2,17 @@ class EventController < ApplicationController
   def index
   end
 
+
+
   def show
   @event = Event.find(params[:id])
   end
 
+before_action :authenticate_user!
   def new
+
+  end
+  def create
 
   end
 end
