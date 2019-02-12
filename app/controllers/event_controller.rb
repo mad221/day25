3e994d900b97db3cb87da9912fece751e04c74e9
totaotata <totaotata@protonmail.com>
2019-02-12 19:45:21 +0100

    Fix Bug

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,14 +1,13 @@
 class EventController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
   end
-
-
 
   def show
   @event = Event.find(params[:id])
   end
 
-before_action :authenticate_user!
   def new
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'event#index'
   resources :event, only: [:index, :new, :show]
   resources :user, only: [:show]
   devise_for :users
-  root 'event#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
