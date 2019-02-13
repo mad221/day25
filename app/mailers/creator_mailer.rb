53c93a926b343e50d3d5fe4df48a568da10e294a
totaotata <totaotata@protonmail.com>
2019-02-13 14:22:26 +0100

    Avant push heroku

--- app/mailers/creator_mailer.rb
+++ app/mailers/creator_mailer.rb
@@ -3,8 +3,8 @@ class CreatorMailer < ApplicationMailer
 
 def send_mail_creator(new_attendee)
 
-  @attendee= new_attendee.user_id
-  @name_attendee= User.find(@attendee).first_name
+  @attendee = new_attendee.user_id
+  @name_attendee = User.find(@attendee).first_name
   creator = Event.find(new_attendee.event_id)
 
   @name_creator = User.find(creator.user_id).first_name
